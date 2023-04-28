a5fc65475644168aebeee83e9ed2d052987ad2c7
JZITNIK-github <kintiz@seznam.cz>
2023-04-28 11:12:56 +0200

    remove cookie preferences

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,7 +8,6 @@ function Footer() {
             <h1>Jakub Žitník</h1>
             <p>© Copyright <strong>Jakub Žitník</strong>. {texty["pravavyhrazena"]}.</p>
             <Link to="/legal">{texty["legalniinfo"]}</Link>
-            <a href="#">{texty["cookiepreference"]}</a>
             <div className="languageSelect">
                 <a onClick={()=>{switchLanguage("english")}}>English</a>
                 <a onClick={()=>{switchLanguage("czech")}}>Czech</a>
