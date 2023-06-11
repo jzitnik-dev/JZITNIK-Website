4ea974aa4c251cfeecee3ff449b7455a7fb17f23
JZITNIK-github <kintiz@seznam.cz>
2023-06-11 13:58:07 +0200

    add hacker text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -18,6 +18,30 @@ function Header() {
     function toggleMenu() {
         document.querySelector(".nav_mobile").classList.toggle("active")
     }
+    function hoverEffect(event) {
+        let iteration = 0;
+        let interval = null;
+        clearInterval(interval);
+        
+        interval = setInterval(() => {
+            event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter, index) => {
+                if(index < iteration) {
+                return event.target.dataset.value[index];
+                }
+            
+                return "abcdefhjijklmnopqrstuvwxyz"[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+            
+            if(iteration >= event.target.dataset.value.length){ 
+            clearInterval(interval);
+            }
+            
+            iteration += 1 / 3;
+        }, 30);
+    }
     return (
         <>
             <div className="nav_mobile">
@@ -40,10 +64,10 @@ function Header() {
                 <header style={{top: "0px"}}>
                     <Link to="/"><div className="name">Jakub Žitník</div></Link>
                     <div className="elements">
-                        <NavLink to="/" className="link">{texty["domov"]}</NavLink>
-                        <NavLink to="/about" className="link">{texty["O mě"]}</NavLink>
-                        <a href="/blog" className="link">Blog</a>
-                        <NavLink to="/certificates" className="link">{texty["certifikaty"]}</NavLink>
+                        <NavLink style={{"width": "50px"}} data-value={texty["domov"]} onMouseOver={hoverEffect} to="/" className="link">{texty["domov"]}</NavLink>
+                        <NavLink style={{"width": "40px"}} data-value={texty["O mě"]} onMouseOver={hoverEffect} to="/about" className="link">{texty["O mě"]}</NavLink>
+                        <a style={{"width": "33px"}} data-value="Blog" onMouseOver={hoverEffect} href="/blog" className="link">Blog</a>
+                        <NavLink style={{"width": "74px"}} data-value={texty["certifikaty"]} onMouseOver={hoverEffect} to="/certificates" className="link">{texty["certifikaty"]}</NavLink>
                     </div>
                     <div className="navIcon" onClick={toggleMenu}></div>
                 </header>
