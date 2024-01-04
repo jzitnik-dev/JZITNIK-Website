84c959851b8287dde8d1655bd3d5febaa61445a9
JZITNIK-github <email@jzitnik.is-a.dev>
2024-01-04 10:51:07 +0100

    Update cloud

--- src/pages/cloud/Body.jsx
+++ src/pages/cloud/Body.jsx
@@ -13,6 +13,29 @@ function cloud() {
   const [content, setContent] = useState(<Loading fallback />);
 
   useEffect(() => {
+    function humanFileSize(bytes, si = false, dp = 1) {
+      const thresh = si ? 1000 : 1024;
+
+      if (Math.abs(bytes) < thresh) {
+        return bytes + " B";
+      }
+
+      const units = si
+        ? ["KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+        : ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+      let u = -1;
+      const r = 10 ** dp;
+
+      do {
+        bytes /= thresh;
+        ++u;
+      } while (
+        Math.round(Math.abs(bytes) * r) / r >= thresh &&
+        u < units.length - 1
+      );
+
+      return bytes.toFixed(dp) + " " + units[u];
+    }
     if (!shouldDownload.current) return;
     shouldDownload.current = false;
     (async () => {
@@ -34,8 +57,11 @@ function cloud() {
                 <div className="aboutmeContent">
                   <h1>{`${percentage.toFixed(2)}%`}</h1>
                   <p>Právě probíhá stahování</p>
+                  <hr />
+                  <p>Název souboru: {file}</p>
+                  <p>Velikost souboru: {humanFileSize(totalSize)}</p>
                 </div>
-              </div>,
+              </div>
             );
           } else {
             console.log(
@@ -65,7 +91,7 @@ function cloud() {
                   <h1>Staženo!</h1>
                   <p>Soubor byl úspěšně stažen!</p>
                 </div>
-              </div>,
+              </div>
             );
           } else if (xhr.status === 403) {
             setContent(
@@ -74,7 +100,7 @@ function cloud() {
                   <h1>Chyba!</h1>
                   <p>K tomuto souboru nemáte přístup!</p>
                 </div>
-              </div>,
+              </div>
             );
           } else if (xhr.status === 404) {
             setContent(
@@ -83,8 +109,18 @@ function cloud() {
                   <h1>Chyba!</h1>
                   <p>Tento soubor nebyl nalezen!</p>
                 </div>
-              </div>,
+              </div>
             );
+          } else if (xhr.status === 410) {
+            setContent(
+              <div className="aboutmeStarting">
+                <div className="aboutmeContent">
+                  <h1>Pozor!</h1>
+                  <p>Platnost odkazu vypršela.</p>
+                  <p>Kontaktujte majitele obsahu pro obnovení odkazu.</p>
+                </div>
+              </div>
+            )
           } else {
             setContent(
               <div className="aboutmeStarting">
@@ -93,7 +129,7 @@ function cloud() {
                   <p>Kód chyby: {xhr.status}</p>
                   <p>Text: {xhr.statusText}</p>
                 </div>
-              </div>,
+              </div>
             );
           }
         };
