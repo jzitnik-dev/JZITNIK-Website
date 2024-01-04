63062c5f924acd9a7e03c04ea76149992d7c6244
JZITNIK-github <email@jzitnik.is-a.dev>
2024-01-04 11:00:50 +0100

    Update backned

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 class Config {
-  // static backend = "https://backend.jzitnik.is-a.dev/"
-  static backend = "http://localhost:1212/"
+  static backend = "https://backend.jzitnik.is-a.dev/"
+  // static backend = "http://localhost:1212/"
 }
 
 export default Config;
